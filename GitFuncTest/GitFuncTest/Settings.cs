0aea00199696b02a85b357057c5bc84e94aad624
sandro93ZG <jedandvatri884@gmail.com>
2022-11-22 16:05:01 +0100

    added global settings

---
+++ GitFuncTest/GitFuncTest/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitFuncTest
+{
+    public class Settings
+    {
+        public string Option1 = "option_1";
+    }
+}
