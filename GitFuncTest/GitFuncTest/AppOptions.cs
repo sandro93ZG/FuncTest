499130daa9b2c5a2bc89a26341fdd65776bf4e25
sndzgb <sandro.stevanovic@gmail.com>
2022-11-22 15:38:34 +0100

    added options

---
+++ GitFuncTest/GitFuncTest/AppOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitFuncTest
+{
+    public class AppOptions
+    {
+        public AppOptions()
+        {
+
+        }
+
+        private int _option1 = 1;
+    }
+}
