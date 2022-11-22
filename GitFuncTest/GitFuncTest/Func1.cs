0ad0bcdf29ae07d8e66419393a162dc7ef91fb7a
sandro93ZG <jedandvatri884@gmail.com>
2022-11-22 16:10:08 +0100

    added func1

---
+++ GitFuncTest/GitFuncTest/Func1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitFuncTest
+{
+    public class Func1
+    {
+        public int MyProperty { get; set; }
+    }
+}
